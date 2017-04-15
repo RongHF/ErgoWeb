05010260bf365397ddc86b2adaab42ce77609caf
Rong Huangfu <rong.huangfu280@gmail.com>
2017-04-14 22:12:37 -0500

    update templates

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<p>Test</p>")
+    return render(request, 'mysite/mysite_header.html')
