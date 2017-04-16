3f8afe4932d82070300355804cf3af1d3ea93277
Rong Huangfu <rong.huangfu280@gmail.com>
2017-04-15 21:06:09 -0500

    update templates

--- mysite/views.py
+++ mysite/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'mysite/mysite_header.html')
+    return render(request, 'mysite/mysite_home.html')
